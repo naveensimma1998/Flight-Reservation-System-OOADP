adf4dc5e3b7744095480c6ee8d6bc8f64846decf
naveen simma <simma naveen>
2021-03-02 20:48:11 +0530

    added to git

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RegularTicket regularTicket = new RegularTicket();
+        TouristTicket touristTicket = new TouristTicket();
+        
+    }
+}
---
+++ src/com/company/TouristTicket.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TouristTicket {
+     String hotelAddress;
+     String[] selectedTouristLocation = new String[5];
+
+
+}
